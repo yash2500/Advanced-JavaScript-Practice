86804b8655fedda2046e78d23021d3c6fa257347
Yash <67836361+yash2500@users.noreply.github.com>
2023-02-14 20:38:39 +0530

    practice1

---
+++ Introduction.js
@@ -0,0 +1,15 @@
+1997 - ECMAScript 1 (European Computer Manufacturers Association)
+1998 - ECMAScript 2 
+1999 - ECMAScript 3  
+2009 - ECMAScript 5 
+2011 - ECMAScript 5.1 
+2015 - ECMAScript 2015 (ES6) // Main Update 
+2016 - ECMAScript 2015 (ES7) 
+2017 - ECMAScript 2015 (ES8) 
+2018 - ECMAScript 2015 (ES9) 
+
+// What we will learn in this tutorial?
+-> JavaScript ES6 and later versions. 
+-> Regular Expressions.
+-> New Advanced Events and Web APIs. 
+-> DOM's new Methods and Properties.
